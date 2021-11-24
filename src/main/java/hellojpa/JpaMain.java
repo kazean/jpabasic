d12a644ab8e64668c0dd2b3534173de8ffc209d5
kazean <yeogunna@gmail.com>
2021-11-24 16:39:52 +0900

    init

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        transaction 단위
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try{
+            /*
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+
+            em.remove(findMember);
+
+            Member findMember = em.find(Member.class,1L);
+            System.out.println("findMember.id = "+ findMember.getId());
+            System.out.println("findMember.name = "+ findMember.getName());
+            findMember.setName("HelloJPA");
+            */
+            List<Member> result = em.createQuery("select m from Member as m",Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(10)
+                    .getResultList();
+            for(Member m : result){
+                System.out.println("member.name = " + m.getName());
+            }
+
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+        emf.close();
+
+    }
+}
